5ae2e43e5284691572527bb910c7683520be79bd
demani.lukas@gmail.com <demani.lukas@gmail.com>
2022-02-10 14:18:59 -0300

    feat: sendToBag

--- src/controllers/bookControllers.js
+++ src/controllers/bookControllers.js
@@ -13,11 +13,52 @@ export async function getBooks(req, res){
 export async function addBook(req, res){
   try {
     const book = req.body;
-    console.log(book);
     db.collection("books").insertOne({ ...book })
 
     res.sendStatus(201)
   } catch {
     res.sendStatus(500)
+  }
+}
+
+export async function searchBook(req, res) {
+  try {
+    const book = req.body;
+    console.log(book.search);
+    const allBooks = await db.collection("books").find({}).toArray();
+    
+    allBooks.filter((book) => book == book)
+    console.group(allBooks);
+
+
+    res.sendStatus(201);
+  } catch {
+    res.sendStatus(500);
+  }
+}
+
+export async function sendToBag(req, res) {
+  try {
+    const book = req.body;
+    
+    db.collection("bag").insertOne({ ...book });
+    res.sendStatus(201)
+  } catch {
+    res.sendStatus(500);
+  }
+}
+
+export async function getBag(req, res) {
+  try {
+    let total = 0;
+    const bag = db.collection("bag").find({}).toArray();
+    for (let i=0; i<bag.lenght; i++){
+      total += bag.price;
+    }
+
+    res.send({ bag, total});
+
+  } catch {
+    res.sendStatus(500);
   }
 }
--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
-import { getBooks, addBook } from "../controllers/bookControllers.js"
+import { getBooks, addBook, searchBook, sendToBag, getBag } from "../controllers/bookControllers.js"
 
 const bookRouter = Router()
 
 bookRouter.get("/books", getBooks);
+bookRouter.post("/search", searchBook);
 bookRouter.post("/books", addBook);
+bookRouter.post("/bag", sendToBag);
+bookRouter.get("/bag", getBag):
 
 export default bookRouter;
